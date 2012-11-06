bfbfb64a8c02ac0a98c2add3ba0b54969278e55c
Liam Kirsher <liamk@numenet.com>
2012-11-06 11:10:42 -0800

    setuptools changes

--- setup.py
+++ setup.py
@@ -1,18 +1,34 @@
-from distutils.core import setup
+#from distutils.core import setup
+import multiprocessing, logging
+from setuptools import setup, find_packages
 
 setup(
-    name='HearPlanetAPI',
-    version='0.1.0',
-    author='Liam Kirsher',
-    author_email='liam@hearplanet.com',
-    packages=['hearplanet', 'hearplanet.test', 'hearplanet.query'],
-    package_data={'':['*.cfg']},
-    data_files=[('/etc/', ['cfg/hearplanet.cfg'])],
-    url='http://pypi.python.org/pypi/HearPlanetAPI/',
-    license='LICENSE.txt',
-    description='HearPlanet API driver',
-    long_description=open('README.rst').read(),
-    requires=[
-        'requests',
-    ],
+    name = 'HearPlanetAPI',
+    version = '0.1.0',
+    description = 'HearPlanet API driver',
+    long_description = open('README.rst').read(),
+    keywords = 'HearPlanet API driver',
+    url = 'http://pypi.python.org/pypi/HearPlanetAPI/',
+    license = 'LICENSE.txt',
+    author = 'Liam Kirsher',
+    author_email = 'liam@hearplanet.com',
+    zip_safe = True,
+
+    packages = find_packages(exclude=['tests', 'examples']),
+    include_package_data = True,
+    data_files = [('/etc/', ['cfg/hearplanet.cfg'])],
+    install_requires = [ 'requests', ],
+
+    tests_require = ['nose'],
+    test_suite = 'nose.collector',
+
+    classifiers = [
+        'Development Status :: 3 - Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ]
 )
